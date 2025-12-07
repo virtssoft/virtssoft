2903308e7f1b37795ebca7296034ac22bbd3631f
virtssoft <compte.virtssoft@gmail.com>
2025-12-07 23:42:28 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
+
+
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
